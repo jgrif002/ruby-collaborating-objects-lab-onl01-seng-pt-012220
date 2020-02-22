08b8346f5c2bacae6803f4517c61972132752abc
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-22 22:50:55 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,12 +1,22 @@
 #require 'pry'
 
 class Mp3_importer 
-  attr_accessor :name 
+  attr_accessor :path 
   @@all = []
   
-  def initialize(name)
-    @name = name 
+  def initialize(path)
+    @name = path 
     @@all << self 
+  end 
+  
+  def files
+    Dir.entries(@path).select {|file| file.include?(".mp3")}
+  end 
+  
+  def import 
+    files.each do |file|
+      Song.new_by_filename(file)
+    end 
   end 
   
   def self.all 
